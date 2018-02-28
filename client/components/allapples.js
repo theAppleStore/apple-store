1e1b0785eca72a7dab2aa4056f7c6d740de4bcdd
Claudia Baik <claudiabaik@gmail.com>
2018-02-27 19:47:14 -0500

    updated category rendering, but fails when refresh

--- client/components/allapples.js
+++ client/components/allapples.js
@@ -1,53 +1,65 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import store, {fetchApples} from '../store'
-import AppleItem from './appleitem'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import store, { fetchApples } from "../store";
+import AppleItem from "./appleitem";
 
 class AllApples extends React.Component {
-  constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
+    this.renderApples = this.renderApples.bind(this);
   }
-  componentDidMount(){
-    console.log("HI")
+  componentDidMount() {
+    console.log("HI");
     this.props.mountApples();
   }
-  render () {
-    console.log('ALLAPPLES',this.props.apples);
+
+  renderApples(category) {
+let filteredList;
+    if (category === 'all') {
+      console.log('RENDER ALL')
+      filteredList = this.props.apples
+    }
+    filteredList = this.props.apples.filter(
+      apple => apple.category === category
+    );
+    console.log('filteredLIst', filteredList)
     return (
       <div>
-        <h1>All Apples</h1>
-
-        <ul className = 'apple-list'>
-        {this.props.apples.map(apple => (
-           <ul key={apple.id}>
-            <AppleItem apple = {apple}/>
-           </ul>
-         )
-        )
-      }
-         </ul>
+        <h1>{category} Apples</h1>
+        <ul className="apple-list">
+          {filteredList.map(apple => (
+            <ul key={apple.id}>
+              <AppleItem apple={apple} />
+            </ul>
+          ))}
+        </ul>
       </div>
-    )
+    );
+  }
+  render() {
+    console.log("ALLAPPLES", this.props);
+    return <div>{this.props.currentCategory && this.renderApples(this.props.currentCategory)}</div>;
   }
 }
 
-const mapStateToProps = function(state){
-  console.log(state)
+const mapStateToProps = function(state) {
+  console.log(state);
   return {
-    apples: state.apples
-  }
-}
+    apples: state.apples,
+    currentCategory: state.currentCategory
+  };
+};
 
-const mapDispatchToProps = function(dispatch){
+const mapDispatchToProps = function(dispatch) {
   return {
-    mountApples: function(){
-     dispatch(
-        fetchApples()
-      )
+    mountApples: function() {
+      dispatch(fetchApples());
     }
-  }
-}
+  };
+};
 
-const AllApplesContainer = connect(mapStateToProps, mapDispatchToProps)(AllApples)
-export default AllApplesContainer
+const AllApplesContainer = connect(mapStateToProps, mapDispatchToProps)(
+  AllApples
+);
+export default AllApplesContainer;
